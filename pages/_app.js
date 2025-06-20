ff323e18039f5891f51308c53df0e2ded4d9a46d
Abhishek-Punhani <punhani.manavabhi@gmail.com>
2025-06-20 22:24:10 +0530

    Added use Effect

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,16 @@
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
-  
-  if (typeof window !== 'undefined' && !router.asPath.startsWith(`/${router.locale}`)) {
-    router.push(`/${router.locale}${router.asPath}`);
-  }
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !router.asPath.startsWith(`/${router.locale}`)) {
+      router.push(`/${router.locale}${router.asPath}`);
+    }
+  }, [router]);
+
   return <Component {...pageProps} />;
 }
 
