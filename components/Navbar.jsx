c5d68aeb0b6ae40a80f4f38d4b73ed113eadaaaf
ShubhamKumar547 <kumarshubh054@gmail.com>
2025-06-21 14:22:03 +0530

    added the navbar and included language toggle button in it

---
+++ components/Navbar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import LanguageSwitcher from "./LanguageSwitcher";
+
+export default function Navbar() {
+  return (
+    <nav className="bg-white shadow-md">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex items-center">
+            <div className="hover:cursor-pointer text-3xl font-bold text-gray-800">
+              UICIL
+            </div>
+            <div className="hover:cursor-pointer hidden md:ml-10 md:flex md:space-x-8">
+              <div className="text-gray-700 hover:text-blue-600 px-3 py-2 text-xl font-medium hover:cursor-pointer">
+                Home
+              </div>
+              <div className="text-gray-700 hover:text-blue-600 px-3 py-2 text-xl font-medium hover:cursor-pointer">
+                Grievance
+              </div>
+              <div className="text-gray-700 hover:text-blue-600 px-3 py-2 text-xl font-medium hover:cursor-pointer">
+                Stories
+              </div>
+            </div>
+          </div>
+
+          <div className="flex items-center">
+            <LanguageSwitcher />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
