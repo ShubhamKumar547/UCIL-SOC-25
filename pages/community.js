e0106c9aa3989ecfdf9c89c1edda09888dd69c84
ShubhamKumar547 <kumarshubh054@gmail.com>
2025-06-22 14:50:47 +0530

    added testimonials,demands of communiity sections

--- pages/community.js
+++ pages/community.js
@@ -3,15 +3,28 @@ import Navbar from "@/components/Navbar";
 import { useRouter } from "next/router";
 import en from "../public/locales/en/common.json";
 import hi from "../public/locales/hi/common.json";
+import CommunityDemands from "@/components/Demands";
+import CommunityTestimonials from "@/components/Testimonials";
 
 const community = () => {
   const translations = { en, hi };
   const { locale } = useRouter();
   const t = translations[locale] || translations.en;
   return (
-    <div>
-      <Navbar t={t} />
-    </div>
+    <>
+      <div>
+        <Navbar t={t} />
+      </div>
+      <div className="max-w-7xl mx-auto px-4  pt-2 ">
+        <CommunityDemands/>
+      </div>
+      <div className="max-w-7xl mx-auto px-4  pt-2 ">
+        <CommunityTestimonials/>
+      </div>
+
+
+
+    </>
   );
 };
 
